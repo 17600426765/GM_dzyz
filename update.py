86d5d4ef3ce20810ff515f46db1adb8972b2e8a3
kunyun <17600426765>
2020-05-23 17:42:52 +0800

    增加日志打印

--- update.py
+++ update.py
@@ -7,19 +7,26 @@
 # * Description   : 
 # **********************************************************
 
+
 import sys
 from fabric import Connection
 #from fabric.api import run
+import logging
+
+
+logging.basicConfig(level=logging.INFO,format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',datefmt='%a, %d %b %Y %H:%M:%S')
 
 def view():
 	print("\t1.更新")
 	print("\tq.退出")
 
 def update_dzyz():
-
+	path_dzyz = '/data/'
 	c = Connection("root@192.168.65.170",connect_kwargs={"password":"123"})
-	with c.cd('/opt'):
-		c.run('ls')
+		
+	
+	with c.cd(path_dzyz):
+		c.run('ls -l')
 	
 
 #调用函数,并进行判断执行的选项
