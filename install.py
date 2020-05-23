fa12e7b139c9d854ab9fb91accb22fdd60bd2bc3
kunyun <17600426765>
2020-05-23 09:37:41 +0800

    打印

---
+++ install.py
@@ -0,0 +1,10 @@
+#!/root/.pyenv/shims/python
+# **********************************************************
+# * Author        : kunyun
+# * Email         : 
+# * Create time   : 2020-05-23 09:25
+# * Filename      : install.py
+# * Description   : 
+# **********************************************************
+
+print("hello, world!")
